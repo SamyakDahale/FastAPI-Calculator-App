9e77742d9cee42c4aeb457db951d3b01889e3b01
Samyak Shitalchand Dahale <samyakdahale123@gmail.com>
2026-01-09 12:14:24 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException, Query
+
+app = FastAPI()
+
+from fastapi.responses import FileResponse
+
+@app.get("/")
+def read_root():
+    return FileResponse("index.html")
+
+@app.get("/style.css")
+def get_style():
+    return FileResponse("style.css")
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"operation": "add", "a": a, "b": b, "result": a + b}
+
+@app.get("/subtract")
+def subtract(a: float, b: float):
+    return {"operation": "subtract", "a": a, "b": b, "result": a - b}
+
+@app.get("/multiply")
+def multiply(a: float, b: float):
+    return {"operation": "multiply", "a": a, "b": b, "result": a * b}
+
+@app.get("/divide")
+def divide(a: float, b: float):
+    if b == 0:
+        raise HTTPException(status_code=400, detail="Division by zero")
+    return {"operation": "divide", "a": a, "b": b, "result": a / b}
